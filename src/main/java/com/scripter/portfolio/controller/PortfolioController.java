398047de36f826ddb7d6ba63898f574460bdcfe0
syndicate <ritambala@live.com>
2025-02-04 12:53:09 +0530

    log-ip

--- src/main/java/com/scripter/portfolio/controller/PortfolioController.java
+++ src/main/java/com/scripter/portfolio/controller/PortfolioController.java
@@ -5,16 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class PortfolioController {
 	
 	
-	private static final Logger log = LoggerFactory.getLogger(PortfolioController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(PortfolioController.class);
 
 	
 	@GetMapping("/")
-	public String name() {
-		log.debug("Loading index page");
+	public String home(HttpServletRequest httpServletRequest) {
+		LOG.info("User IP: {}",httpServletRequest.getRemoteHost());
 		return "index";
 	}
 }
