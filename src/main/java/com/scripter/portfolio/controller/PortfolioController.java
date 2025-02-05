8bb41dd70bb13270154a2d7ef51a68d642127b0e
syndicate <ritambala@live.com>
2025-02-05 11:02:30 +0530

    new api

--- src/main/java/com/scripter/portfolio/controller/PortfolioController.java
+++ src/main/java/com/scripter/portfolio/controller/PortfolioController.java
@@ -1,11 +1,16 @@
 package com.scripter.portfolio.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class PortfolioController {
@@ -19,4 +24,11 @@ public class PortfolioController {
 		LOG.info("User IP: {}",httpServletRequest.getRemoteHost());
 		return "index";
 	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String sayHello() throws UnknownHostException {
+		return "Hostname: " + InetAddress.getLocalHost().getHostName() + " Address: " + InetAddress.getLocalHost().getHostAddress();
+	}
+	
 }
